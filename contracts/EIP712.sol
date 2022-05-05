f8b0a78c4d068cb019b1df4c9c5219879bfe2e0b
Rubanrubi <ruban@sparkouttech.com>
2022-05-05 12:57:43 +0530

    formating code

--- contracts/EIP712.sol
+++ contracts/EIP712.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
@@ -9,18 +8,22 @@ import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-
-contract TestEIP714 is Initializable, UUPSUpgradeable, EIP712Upgradeable, OwnableUpgradeable {
+contract TestEIP714 is
+    Initializable,
+    UUPSUpgradeable,
+    EIP712Upgradeable,
+    OwnableUpgradeable
+{
     using ECDSAUpgradeable for bytes32;
 
-
-    bytes32 constant TRANSFER_TYPEHASH = keccak256(
-        "Transfer(address seller,address tokenAddress,uint256 amount,address buyer)"
-    );
+    bytes32 constant TRANSFER_TYPEHASH =
+        keccak256(
+            "Transfer(address seller,address tokenAddress,uint256 amount,address buyer)"
+        );
 
     IERC20Upgradeable public token;
 
-     struct Transfer {
+    struct Transfer {
         address seller;
         address tokenAddress;
         uint256 amount;
@@ -28,33 +31,52 @@ contract TestEIP714 is Initializable, UUPSUpgradeable, EIP712Upgradeable, Ownabl
     }
 
     function initialize() public initializer {
-        __Ownable_init_unchained(); 
+        __Ownable_init_unchained();
         __EIP712_init_unchained("Order", "1");
         token = IERC20Upgradeable(0x9fdfE0c1e02BA989ef80C86C4e3393B92314AC87);
-        
     }
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
-    
-    function getTokens(Transfer memory data ,address buyer, bytes calldata signature) external {
+
+    function getTokens(
+        Transfer memory data,
+        address buyer,
+        bytes calldata signature
+    ) external {
         // verify signature
         bytes32 structHash = genBidOrderHash(data);
         bytes32 hashTypedData = _hashTypedDataV4(structHash);
         address signer = verifySignature(hashTypedData, signature);
-        require(signer == buyer, "Exchange: Buy order is not signed by the buyer");
+        require(
+            signer == buyer,
+            "Exchange: Buy order is not signed by the buyer"
+        );
         token.transferFrom(buyer, msg.sender, 10000000000000000000);
         // emit Transfer(buyer, msg.sender, 10000000000000000000);
     }
 
-    function genBidOrderHash(Transfer memory data) internal pure returns(bytes32) {
-        return keccak256(
-            abi.encode(TRANSFER_TYPEHASH, data.seller, data.tokenAddress, data.amount, data.buyer)
-        );
+    function genBidOrderHash(Transfer memory data)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encode(
+                    TRANSFER_TYPEHASH,
+                    data.seller,
+                    data.tokenAddress,
+                    data.amount,
+                    data.buyer
+                )
+            );
     }
 
-    function verifySignature(bytes32 hash, bytes calldata signature) internal pure returns(address) {
+    function verifySignature(bytes32 hash, bytes calldata signature)
+        internal
+        pure
+        returns (address)
+    {
         return ECDSAUpgradeable.recover(hash, signature);
     }
-
-    
 }
